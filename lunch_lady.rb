4c0c1dd8daf51f1117cb6aa89bd2740d92f05b8d
Eric Durr <emdurr@gmail.com>
2016-08-08 07:51:39 -0600

    wallet working

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,19 +1,30 @@
 require 'pry'
 class LunchLady
   attr_accessor :main_menu_items, :counter, :side_dish
-  def initialize
-    @main_menu_items = [['Meatloaf', 5.00], ['Mystery Meat', 3.00], ['Slop', 1.00]]
+  def initialize(wallet_amount = 10.0)
+    @main_menu_items = [['Meatloaf', 5.00],
+                        ['Mystery Meat', 3.00],
+                        ['Slop', 1.00]]
     @main_dish_choice_int = []
     @main_dish_cost = []
     @counter = 1
-    @side_dish = [['Carrots', 1.75], ['Mystery Yogert', 1.00], ['Beef Jerkey', 0.50]]
+    @side_dish = [['Carrots', 1.75],
+                  ['Mystery Yogert', 1.00],
+                  ['Beef Jerkey', 0.50]]
     @side_dish_choice_int = []
     @side_dish_cost = []
     @arr = [1, 2, 3]
+    @wallet_amount = 10
   end
 
   def prompt
     print "-->"
+  end
+
+  def wallet
+    puts "What do you want to your current wallet amount to be?"
+    prompt
+    @wallet_amount << gets.to_f
   end
 
   def main_dish
@@ -37,7 +48,16 @@ class LunchLady
     if @arr.include? user_input_mod
       @main_dish_choice_int << @main_menu_items[user_input_mod - 1][0]
       @main_dish_cost << @main_menu_items[user_input_mod - 1][1]
-      side_dish_menu
+      binding.pry
+      current_total = @main_dish_cost.reduce(0, :+)
+      if current_total > @wallet_amount
+        puts "You do not have enough money for your current selections please start again.\n"
+        @main_dish_cost.clear
+        @main_dish_choice_int.clear
+        main_dish
+      else
+        side_dish_menu
+      end
     else
       puts "** Invalid Main Dish choice! Please try again. **"
       main_dish
@@ -86,6 +106,17 @@ class LunchLady
       if @arr.include? user_input_mod
         @side_dish_choice_int << @side_dish[user_input_mod - 1][0]
         @side_dish_cost << @side_dish[user_input_mod - 1][1]
+        current_total = @main_dish_cost.reduce(0, :+) + @side_dish_cost.reduce(0, :+)
+        if current_total > @wallet_amount
+          puts "You do not have enough money for your current selections please start again."
+          @main_dish_cost.clear
+          @main_dish_choice_int.clear
+          @side_dish_choice_int.clear
+          @side_dish_cost.clear
+          main_dish
+        else
+
+        end
         added += 1
       else
         puts "** Invalid Side Dish choice! Please try again. **"
