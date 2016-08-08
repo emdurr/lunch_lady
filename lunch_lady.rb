46772646a1ebc9603de59bd9c12906b606b0f368
Eric Durr <emdurr@gmail.com>
2016-08-08 07:26:54 -0600

    working program with all but wallet and extra menu item information

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -3,10 +3,13 @@ class LunchLady
   attr_accessor :main_menu_items, :counter, :side_dish
   def initialize
     @main_menu_items = [['Meatloaf', 5.00], ['Mystery Meat', 3.00], ['Slop', 1.00]]
+    @main_dish_choice_int = []
+    @main_dish_cost = []
     @counter = 1
     @side_dish = [['Carrots', 1.75], ['Mystery Yogert', 1.00], ['Beef Jerkey', 0.50]]
     @side_dish_choice_int = []
     @side_dish_cost = []
+    @arr = [1, 2, 3]
   end
 
   def prompt
@@ -14,6 +17,7 @@ class LunchLady
   end
 
   def main_dish
+    puts "Enter 'clear' at any time to start over or 'exit' to quit"
     puts "What main dish would you like?"
     @main_menu_items.each do |el|
       puts "#{counter}: #{el[0]}(#{el[1]})"
@@ -21,10 +25,18 @@ class LunchLady
     end
     @counter = 1
     prompt
-    user_input = gets.to_i
-    @arr = [1, 2, 3]
-    if @arr.include? user_input
-      @main_dish_choice_int_1 = user_input
+    user_input = gets.strip.downcase
+    if user_input == 'clear'
+      puts "Your options have been cleared!"
+      main_dish
+    elsif user_input == 'exit'
+      exit
+    else
+      user_input_mod = user_input.to_i
+    end
+    if @arr.include? user_input_mod
+      @main_dish_choice_int << @main_menu_items[user_input_mod - 1][0]
+      @main_dish_cost << @main_menu_items[user_input_mod - 1][1]
       side_dish_menu
     else
       puts "** Invalid Main Dish choice! Please try again. **"
@@ -34,7 +46,23 @@ class LunchLady
 
   def side_dish_menu
     puts "How many side dishes do you want?"
-    number_to_add = gets.to_i
+    puts "Enter 'clear' to start your side dishes over,"
+    puts "'back' to go back to the main dish menu,"
+    puts "or 'exit' to quit the program."
+    prompt
+    user_input_1 = gets.strip.downcase
+    if user_input_1 == 'clear'
+      puts "Your options have been cleared!"
+      side_dish_menu
+    elsif user_input_1 == 'back'
+      @main_dish_cost.clear
+      @main_dish_choice_int.clear
+      main_dish
+    elsif user_input_1 == 'exit'
+      exit
+    else
+      number_to_add = user_input_1.to_i
+    end
     added = 0
     number_to_add.times do
       puts "Which is your side dish choice?"
@@ -44,10 +72,20 @@ class LunchLady
       end
       @counter = 1
       prompt
-      user_input = gets.to_i
-      if @arr.include? user_input
-        @side_dish_choice_int << @side_dish[user_input - 1][0]
-        @side_dish_cost << @side_dish[user_input - 1][1]
+      user_input = gets.strip.downcase
+      if user_input == 'clear'
+        puts "Your options have been cleared!"
+        @side_dish_choice_int.clear
+        @side_dish_cost.clear
+        added = 0
+        break
+        side_dish_menu
+      else
+        user_input_mod = user_input.to_i
+      end
+      if @arr.include? user_input_mod
+        @side_dish_choice_int << @side_dish[user_input_mod - 1][0]
+        @side_dish_cost << @side_dish[user_input_mod - 1][1]
         added += 1
       else
         puts "** Invalid Side Dish choice! Please try again. **"
@@ -62,15 +100,28 @@ class LunchLady
   end
 
   def summary
-    main_dish_sum = @main_menu_items[@main_dish_choice_int_1 - 1][0]
-    main_dish_cost = @main_menu_items[@main_dish_choice_int_1 - 1][1]
     puts "Your order consists of:"
-    puts "#{main_dish_sum}, #{@side_dish_choice_int.join(", ")}"
-    puts "Your order total is #{main_dish_cost + @side_dish_cost.reduce(0, :+)}"
+    puts "#{@main_dish_choice_int.join(", ")}, #{@side_dish_choice_int.join(", ")}"
+    puts "Your order total is #{@main_dish_cost.reduce(0, :+) + @side_dish_cost.reduce(0, :+)}"
+    @main_dish_cost.clear
+    @main_dish_choice_int.clear
+    @side_dish_choice_int.clear
+    @side_dish_cost.clear
   end
 
 end
 
 lunch_lady = LunchLady.new
 
-lunch_lady.main_dish
+
+
+
+while true
+  user_input = lunch_lady.main_dish
+  if user_input == 'QUIT'
+    puts "Thanks for using your Lunch Lady ordering system. Please come again!"
+    exit
+  else
+    puts
+  end
+end
